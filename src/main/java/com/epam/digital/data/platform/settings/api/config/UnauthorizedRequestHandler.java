f9f9768db443707ed1468872ce00c8f531232776
Yevhen Zvarych <yevhen_zvarych@epam.com>
2022-02-15 15:52:19 +0000

    [EDP-DDM-8109] - Updated security starter version to enable CSRF

--- src/main/java/com/epam/digital/data/platform/settings/api/config/UnauthorizedRequestHandler.java
+++ src/main/java/com/epam/digital/data/platform/settings/api/config/UnauthorizedRequestHandler.java
@@ -16,53 +16,34 @@
 
 package com.epam.digital.data.platform.settings.api.config;
 
-import com.epam.digital.data.platform.settings.api.model.DetailedErrorResponse;
-import com.epam.digital.data.platform.settings.api.audit.RestAuditEventsFacade;
-import com.epam.digital.data.platform.settings.api.service.TraceService;
-import com.epam.digital.data.platform.settings.api.utils.ResponseCode;
-import com.epam.digital.data.platform.starter.audit.model.EventType;
-import com.epam.digital.data.platform.starter.security.jwt.RestAuthenticationEntryPoint;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
-public class UnauthorizedRequestHandler extends RestAuthenticationEntryPoint {
+public class UnauthorizedRequestHandler implements AuthenticationEntryPoint, AccessDeniedHandler {
 
-  private final TraceService traceService;
-  private final ObjectMapper objectMapper;
-  private final RestAuditEventsFacade restAuditEventsFacade;
+  private final HandlerExceptionResolver handlerExceptionResolver;
 
-  public UnauthorizedRequestHandler(
-      ObjectMapper objectMapper,
-      TraceService traceService,
-      RestAuditEventsFacade restAuditEventsFacade) {
-    super(objectMapper);
-    this.traceService = traceService;
-    this.objectMapper = objectMapper;
-    this.restAuditEventsFacade = restAuditEventsFacade;
+  public UnauthorizedRequestHandler(HandlerExceptionResolver handlerExceptionResolver) {
+    this.handlerExceptionResolver = handlerExceptionResolver;
   }
 
   @Override
-  @SuppressWarnings("findsecbugs:XSS_SERVLET")
-  public void commence(HttpServletRequest request, HttpServletResponse response,
-      AuthenticationException e) throws IOException {
-    restAuditEventsFacade.sendExceptionAudit(EventType.SECURITY_EVENT, ResponseCode.AUTHENTICATION_FAILED);
-    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    response.getWriter().write(objectMapper.writeValueAsString(
-        newDetailedResponse(ResponseCode.AUTHENTICATION_FAILED)));
+  public void commence(
+          HttpServletRequest request, HttpServletResponse response, AuthenticationException e) {
+    handlerExceptionResolver.resolveException(request, response, null, e);
   }
 
-  private <T> DetailedErrorResponse<T> newDetailedResponse(String code) {
-    var response = new DetailedErrorResponse<T>();
-    response.setTraceId(traceService.getRequestId());
-    response.setCode(code);
-    return response;
+  @Override
+  public void handle(
+          HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) {
+    handlerExceptionResolver.resolveException(request, response, null, e);
   }
 }
